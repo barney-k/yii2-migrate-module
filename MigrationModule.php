912960946fed919bb16cac9d5c8720bc1fe4f770
barney-k <info@barney-k.com>
2016-02-21 14:50:26 +0100

    Adding PHPDoc

--- MigrationModule.php
+++ MigrationModule.php
@@ -1,17 +1,40 @@
 <?php
+
+/**
+ * @package   yii2-migration-module
+ * @author    Barney K <info@barney-k.com>
+ * @version   1.0.0
+ */
+ 
 namespace barneyk\migration;
 
 use Yii;
 use yii\filters\AccessControl;
 
+/**
+ * Migration module for managing migrations
+ *
+ * @author Barney K <info@barney-k.com>
+ */
+ 
 class MigrationModule extends \yii\base\Module
 {
+	/** @var array */
 	public $admins = [];
+	
+	/** @var string */
 	public $defaultRoute = 'items/index';
+	
+	/** @var string */
 	public $migrationPath = '@vendor/barney-k/yii2-migration-module/migrations';
+	
+	/** @var string */
 	public $dateFormat = 'Y.m.d. H:i:s';
+	
+	/** @var string */
 	public $migrationTable = 'migration';
 	
+	/** @inheritdoc */
     public function behaviors()
     {
         return [
@@ -30,6 +53,7 @@ class MigrationModule extends \yii\base\Module
         ];
     }
 	
+	/** @inheritdoc */
 	public function init(){
 		parent::init();
 		
